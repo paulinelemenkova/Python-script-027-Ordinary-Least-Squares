b9a2605d49f07121a7c9af5cecec48158e384162
Pauline Lemenkova <pauline.lemenkova@gmail.com>
2019-05-30 10:15:20 +0800

    Add files via upload

---
+++ Script-027-SM-Ordinary Least Squares.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[4]:
+
+from __future__ import print_function
+import numpy as np
+import pandas as pd
+import statsmodels.api as sm
+from patsy import dmatrices
+import os
+os.chdir('/Users/pauline/Documents/Python')
+df = pd.read_csv("Tab-Morph.csv")
+y, X = dmatrices('profile ~ sedim_thick + igneous_volc + slope_angle', 
+                 data=df, return_type='dataframe')
+mod = sm.OLS(y, X)
+res = mod.fit()
+print(res.summary())
+res.params
+res.rsquared
+sm.stats.linear_rainbow(res)
+
+ # In[ ]:
+
+
+
+
